0794d241e09cd093c1949519a61c6ab4edb02962
ghettoshogun <93882445+ghettoshogun@users.noreply.github.com>
2023-02-09 15:11:56 -0800

    Add files via upload

---
+++ YOUNG MASTER/P.cpp
@@ -0,0 +1,19 @@
+
+int hud = int (*)[(0)+(this)](*);
+void  void([0] <{%}> (:&~~:)) (:)(void->[&(int)]>[(int)]->(~[=]~))->(:)->(hud);
+auto *trap = &hud;
+
+static void __hunter: operator(->[&hud(operator)(&)]:[&])
+{
+    :void:[(yest), [mest], (test)] -> [~]->[test]:[mest]<(22):
+    mest+(hud(++)=&hud(void int->(0):[hud(&)])+(1)*)->{return (0):(0):&(hud&(1))};
+
+    throw: this(operator:hud(0):(&[88]<&(hud[88])));
+}
+
+void operator (hud(++)):(&this)->[mest];
+static void __manna[1]->(&this)(0)>(test):(++);
+hud(++)->(void (__manna)::&((&this))->(mest:[&]:(0))):->(&(~yest),(~void(&)));
+
+operator[:return (0)&&(2):->(&[&operator=([%]->this.const(yest.(&this->[&]<[yest:(hud&)])))])];
+
